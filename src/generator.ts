dc30c345c97b61e09f71059c76ade8f001e44d1c
Martin <tincho.nieves@gmail.com>
2019-06-27 11:20:10 -0300

    Interface generation and error messages
    - Interface type generation fixed
    - Error messages improved

--- src/generator.ts
+++ src/generator.ts
@@ -1,36 +1,37 @@
 import * as child_process from 'child_process';
 import { singular } from 'pluralize';
-import * as colors from 'colors';
+import { Color, red, green, yellow } from 'colors';
+
 
 export class Generator {
 
     private static args: string[] = process && process.argv && process.argv.slice(2) || [];
 
-    private static run(cmd: string): Promise<any> {
-        return new Promise((resolve, reject) => child_process.exec(cmd, (error, stdout) => error ? reject(error) : resolve(stdout)));
+    private static run(cmd: string): Promise<string> {
+        return new Promise<string>((resolve, reject) => child_process.exec(cmd, (error, stdout) => error ? reject(error) : resolve(stdout)));
     }
 
-    private static outputError(txt: string): string {
-        return colors.red(`[ngx-extended-module-generator] ${txt}`);
+    private static output(txt: string, color: Color): string {
+        return color(`[ngx-extended-module-generator] ${txt}`);
     }
 
-    private static outputSuccess(txt: string): string {
-        return colors.green(`[ngx-extended-module-generator] ${txt}`);
+    private static breakLineFormat(txt: string, color: Color): string {
+        return txt.split(/\r?\n/).filter((x: string) => x).map((txt: string) => `\n${this.output(txt, color)}`).join('');
     }
 
     public static async createModule(): Promise<void> {
         try {
             if (!this.args[0])
-                return console.error(this.outputError('Invalid module path or name.'));
+                return console.error(this.output(`Module path or name is required. Example usage: ngex modules/users`, yellow));
             const modulePath = this.args[0];
             const moduleName = modulePath.indexOf('/') != -1 ? modulePath.split('/')[1] : modulePath;
-            console.log(this.outputSuccess(await this.run(`ng generate module ${modulePath} --routing`)));
-            console.log(this.outputSuccess(await this.run(`ng generate component ${modulePath}/pages/${moduleName}`)));
-            console.log(this.outputSuccess(await this.run(`ng generate service ${modulePath}/services/${moduleName}`)));
-            console.log(this.outputSuccess(await this.run(`ng generate interface ${modulePath}/models/${singular(moduleName)}.model`)));
+            console.log(this.breakLineFormat(await this.run(`ng generate module ${modulePath} --routing`), green));
+            console.log(this.breakLineFormat(await this.run(`ng generate component ${modulePath}/pages/${moduleName}`), green));
+            console.log(this.breakLineFormat(await this.run(`ng generate service ${modulePath}/services/${moduleName}`), green));
+            console.log(this.breakLineFormat(await this.run(`ng generate interface ${modulePath}/models/${singular(moduleName)} model`), green));
         } catch (err) {
-            const errMessage: string = err.message.split(/\r?\n/).filter((x: string) => x).map((txt: string) => `\n${this.outputError(txt)}`).join('');
-            return console.error(errMessage);
+            const errMessage: string = err.message || "Something went wrong. Verify that you are in an Angular project.";
+            return console.error(this.breakLineFormat(errMessage, red));
         }
     }
 }
